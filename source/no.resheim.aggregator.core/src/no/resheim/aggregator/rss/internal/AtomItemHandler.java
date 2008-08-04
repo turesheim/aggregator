7d5377e74b61e9614ba4f25501d5d65cbdd68e3b
Torkild U. Resheim <torkildr@gmail.com>
2008-08-04 16:19:30 +0000

    Code clean up.

--- source/no.resheim.aggregator.core/src/no/resheim/aggregator/rss/internal/AtomItemHandler.java
+++ source/no.resheim.aggregator.core/src/no/resheim/aggregator/rss/internal/AtomItemHandler.java
@@ -39,8 +39,6 @@ public class AtomItemHandler extends AbstractElementHandler {
 
 	private static final String SUMMARY = "summary"; //$NON-NLS-1$
 
-	private static final String GUID = "guid"; //$NON-NLS-1$
-
 	/** WordPress full content element */
 	private static final String CONTENT = "content"; //$NON-NLS-1$
 
@@ -48,27 +46,27 @@ public class AtomItemHandler extends AbstractElementHandler {
 			"yyyy-MM-ddEEE, d MMM yyyy HH:mm:ss Z"); //$NON-NLS-1$
 
 	static String[] date_formats = {
-			"yyyy-MM-dd'T'kk:mm:ssZ", // ISO
-			"yyyy-MM-dd'T'kk:mm:ss'Z'", // ISO
-			"yyyy-MM-dd'T'kk:mm:ssz", // ISO
-			"yyyy-MM-dd'T'kk:mm:ss", // ISO
-			"EEE, d MMM yy kk:mm:ss z", // RFC822
-			"EEE, d MMM yyyy kk:mm:ss z", // RFC2882
-			"EEE MMM  d kk:mm:ss zzz yyyy", // ASC
-			"EEE, dd MMMM yyyy kk:mm:ss", // Mon, 26 January 2004
+			"yyyy-MM-dd'T'kk:mm:ssZ", // ISO //$NON-NLS-1$
+			"yyyy-MM-dd'T'kk:mm:ss'Z'", // ISO //$NON-NLS-1$
+			"yyyy-MM-dd'T'kk:mm:ssz", // ISO //$NON-NLS-1$
+			"yyyy-MM-dd'T'kk:mm:ss", // ISO //$NON-NLS-1$
+			"EEE, d MMM yy kk:mm:ss z", // RFC822 //$NON-NLS-1$
+			"EEE, d MMM yyyy kk:mm:ss z", // RFC2882 //$NON-NLS-1$
+			"EEE MMM  d kk:mm:ss zzz yyyy", // ASC //$NON-NLS-1$
+			"EEE, dd MMMM yyyy kk:mm:ss", // Mon, 26 January 2004 //$NON-NLS-1$
 			// 16:31:00 ET
-			"yyyy-MM-dd kk:mm:ss.0", "-yy-MM", "-yyMM", "yy-MM-dd",
-			"yyyy-MM-dd", "yyyy-MM", "yyyy-D", "-yyMM", "yyyyMMdd", "yyMMdd",
-			"yyyy", "yyD"
+			"yyyy-MM-dd kk:mm:ss.0", "-yy-MM", "-yyMM", "yy-MM-dd", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			"yyyy-MM-dd", "yyyy-MM", "yyyy-D", "-yyMM", "yyyyMMdd", "yyMMdd", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+			"yyyy", "yyD" //$NON-NLS-1$ //$NON-NLS-2$
 
 	};
 
 	public static Date parse(String d) {
 		Date date = new Date();
 		SimpleDateFormat formatter = new SimpleDateFormat();
-		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-		d = d.replaceAll("([-+]\\d\\d:\\d\\d)", "GMT$1"); // Correct W3C times
-		d = d.replaceAll(" ([ACEMP])T$", " $1ST"); // Correct Disney times
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT")); //$NON-NLS-1$
+		d = d.replaceAll("([-+]\\d\\d:\\d\\d)", "GMT$1"); // Correct W3C times //$NON-NLS-1$ //$NON-NLS-2$
+		d = d.replaceAll(" ([ACEMP])T$", " $1ST"); // Correct Disney times //$NON-NLS-1$ //$NON-NLS-2$
 		for (int i = 0; i < date_formats.length; i++) {
 			try {
 				formatter.applyPattern(date_formats[i]);
