1cd666eca843089c368dbc5387547446f8e5c565
Torkild U. Resheim <torkildr@gmail.com>
2008-08-19 19:44:05 +0000

    Improved Atom handling. Still something left though.

--- source/no.resheim.aggregator.core/src/no/resheim/aggregator/rss/internal/AtomItemHandler.java
+++ source/no.resheim.aggregator.core/src/no/resheim/aggregator/rss/internal/AtomItemHandler.java
@@ -29,7 +29,76 @@ import org.xml.sax.SAXException;
  * 
  */
 public class AtomItemHandler extends AbstractElementHandler {
+	/*
+	The "atom:entry" element represents an individual entry, acting as a
+	container for metadata and data associated with the entry.  This
+	element can appear as a child of the atom:feed element, or it can
+	appear as the document (i.e., top-level) element of a stand-alone
+	Atom Entry Document.
+	
+	   atomEntry =
+	      element atom:entry {
+	         atomCommonAttributes,
+	         (atomAuthor*
+	          & atomCategory*
+	          & atomContent?
+	          & atomContributor*
+	          & atomId
+	          & atomLink*
+	          & atomPublished?
+	          & atomRights?
+	          & atomSource?
+	          & atomSummary?
+	          & atomTitle
+	          & atomUpdated
+	          & extensionElement*)
+	      }
 
+	This specification assigns no significance to the order of appearance
+	of the child elements of atom:entry.
+
+	The following child elements are defined by this specification (note
+	that it requires the presence of some of these elements):
+
+	o  atom:entry elements MUST contain one or more atom:author elements,
+	  unless the atom:entry contains an atom:source element that
+	  contains an atom:author element or, in an Atom Feed Document, the
+	  atom:feed element contains an atom:author element itself.
+	o  atom:entry elements MAY contain any number of atom:category
+	  elements.
+	o  atom:entry elements MUST NOT contain more than one atom:content
+	  element.
+	o  atom:entry elements MAY contain any number of atom:contributor
+	  elements.
+	o  atom:entry elements MUST contain exactly one atom:id element.
+	o  atom:entry elements that contain no child atom:content element
+	  MUST contain at least one atom:link element with a rel attribute
+	  value of "alternate".
+	o  atom:entry elements MUST NOT contain more than one atom:link
+	  element with a rel attribute value of "alternate" that has the
+	  same combination of type and hreflang attribute values.
+	o  atom:entry elements MAY contain additional atom:link elements
+	  beyond those described above.
+	o  atom:entry elements MUST NOT contain more than one atom:published
+	  element.
+	o  atom:entry elements MUST NOT contain more than one atom:rights
+	  element.
+	o  atom:entry elements MUST NOT contain more than one atom:source
+	  element.
+	o  atom:entry elements MUST contain an atom:summary element in either
+	  of the following cases:
+	  *  the atom:entry contains an atom:content that has a "src"
+	     attribute (and is thus empty).
+	  *  the atom:entry contains content that is encoded in Base64;
+	     i.e., the "type" attribute of atom:content is a MIME media type
+	     [MIMEREG], but is not an XML media type [RFC3023], does not
+	     begin with "text/", and does not end with "/xml" or "+xml".
+	o  atom:entry elements MUST NOT contain more than one atom:summary
+	  element.
+	o  atom:entry elements MUST contain exactly one atom:title element.
+	o  atom:entry elements MUST contain exactly one atom:updated element.
+	  
+	*/
 	private static final String ENTRY = "entry"; //$NON-NLS-1$
 
 	private static final String ISSUED = "issued"; //$NON-NLS-1$
@@ -37,6 +106,8 @@ public class AtomItemHandler extends AbstractElementHandler {
 	private static final String ID = "id"; //$NON-NLS-1$
 
 	private static final String SUMMARY = "summary"; //$NON-NLS-1$
+
+	private Attributes attributes;
 
 	/** WordPress full content element */
 	private static final String CONTENT = "content"; //$NON-NLS-1$
@@ -54,6 +125,7 @@ public class AtomItemHandler extends AbstractElementHandler {
 
 	public void endElement(String qName) throws SAXException {
 		super.endElement(qName);
+		// Mandatory
 		if (qName.equals(TITLE)) {
 			item.setTitle(getBuffer().toString());
 			setCapture(false);
@@ -63,10 +135,7 @@ public class AtomItemHandler extends AbstractElementHandler {
 			setCapture(false);
 		}
 		if (qName.equals(SUMMARY)) {
-			// Make sure we don't overwrite description added by content:encoded
-			if (item.getDescription() == null) {
-				item.setDescription(getBuffer().toString());
-			}
+			item.setDescription(getBuffer().toString());
 			setCapture(false);
 		}
 		if (qName.equals(CONTENT)) {
@@ -92,12 +161,15 @@ public class AtomItemHandler extends AbstractElementHandler {
 	public IElementHandler startElement(String qName, Attributes atts)
 			throws SAXException {
 		super.startElement(qName, atts);
-		if (qName.equals(TITLE) || qName.equals(LINK)
-				|| qName.equals(DESCRIPTION) || qName.equals(ISSUED)
-				|| qName.equals(ID) || qName.equals(CONTENT)) {
+		if (qName.equals(TITLE) || qName.equals(SUMMARY)
+				|| qName.equals(ISSUED) || qName.equals(ID)
+				|| qName.equals(CONTENT) || qName.equals("updated")) {
 			setCapture(true);
+		}
+		// Must exist if there is not a "content" element
+		if (qName.equals(LINK)) {
+			item.setLink(atts.getValue("href")); //$NON-NLS-1$
 		}
 		return this;
 	}
-
 }
