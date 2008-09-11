e822669325f129ef1384af4571e975b3cfc7e580
Torkild U. Resheim <torkildr@gmail.com>
2008-09-11 19:22:37 +0000

    Cleaned up a bit.

--- source/no.resheim.aggregator.core.ui/src/no/resheim/aggregator/core/ui/commands/CollectionParameterValueConverter.java
+++ source/no.resheim.aggregator.core.ui/src/no/resheim/aggregator/core/ui/commands/CollectionParameterValueConverter.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Torkild Ulvøy Resheim.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Torkild Ulvøy Resheim - initial API and implementation
+ *******************************************************************************/
 package no.resheim.aggregator.core.ui.commands;
 
 import no.resheim.aggregator.AggregatorPlugin;
@@ -5,12 +16,20 @@ import no.resheim.aggregator.AggregatorPlugin;
 import org.eclipse.core.commands.AbstractParameterValueConverter;
 import org.eclipse.core.commands.ParameterValueConversionException;
 
+/**
+ * 
+ * @author Torkild Ulvøy Resheim
+ * @since 1.0
+ */
 public class CollectionParameterValueConverter extends
 		AbstractParameterValueConverter {
 
 	public CollectionParameterValueConverter() {
 	}
 
+	/**
+	 * Returns the feed collection with the given identifier.
+	 */
 	@Override
 	public Object convertToObject(String parameterValue)
 			throws ParameterValueConversionException {
@@ -20,8 +39,6 @@ public class CollectionParameterValueConverter extends
 	@Override
 	public String convertToString(Object parameterValue)
 			throws ParameterValueConversionException {
-		System.out.println(parameterValue.getClass().toString());
 		return parameterValue.toString();
 	}
-
 }
