0db106524ff2e081d3f7a87fa9def0fda462903f
Torkild U. Resheim <torkildr@gmail.com>
2008-12-20 20:24:01 +0000

    Updated scripts.

--- releng/ant.sh
+++ releng/ant.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/bin/java -jar /opt/eclipse_3.4/plugins/org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar -application org.eclipse.ant.core.antRunner "$@"
+ANT_CMD="/usr/bin/java -jar /opt/eclipse_3.4/plugins/org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar -application org.eclipse.ant.core.antRunner"
+exec "$ANT_CMD" "$@"
