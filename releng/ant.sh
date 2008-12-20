f9ef8e0d4f222b01af001fefd8a26c86ae802600
Torkild U. Resheim <torkildr@gmail.com>
2008-12-20 20:14:48 +0000

    Adds arguments.

--- releng/ant.sh
+++ releng/ant.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/java -jar /opt/eclipse_3.4/plugins/org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar -application org.eclipse.ant.core.antRunner
+/usr/bin/java -jar /opt/eclipse_3.4/plugins/org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar -application org.eclipse.ant.core.antRunner "$@"
