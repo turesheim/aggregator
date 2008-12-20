8b9fcde62fb570793dd440038dede871c5798198
Torkild U. Resheim <torkildr@gmail.com>
2008-12-20 22:01:57 +0000

    Minor adjustments.

--- releng/ant.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ANT_CMD="/usr/bin/java -jar /opt/eclipse_3.4/plugins/org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar -application org.eclipse.ant.core.antRunner"
-exec "$ANT_CMD" "$@"
