5a5e92c27c2db79188095b6e96e2142601265d55
Torkild U. Resheim <torkildr@gmail.com>
2009-03-04 21:31:43 +0000

    Comments.

--- source/no.resheim.aggregator.core/src/no/resheim/aggregator/core/rss/internal/AbstractItemHandler.java
+++ source/no.resheim.aggregator.core/src/no/resheim/aggregator/core/rss/internal/AbstractItemHandler.java
@@ -1,7 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2009 Torkild Ulvøy Resheim.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Torkild Ulvøy Resheim - initial API and implementation
+ *******************************************************************************/
 package no.resheim.aggregator.core.rss.internal;
 
 import no.resheim.aggregator.core.data.Article;
 
+/**
+ * 
+ * @author Torkild Ulvøy Resheim
+ * @since 1.0
+ */
 public class AbstractItemHandler extends AbstractElementHandler {
 
 	private boolean hasArticle(Article article) {
