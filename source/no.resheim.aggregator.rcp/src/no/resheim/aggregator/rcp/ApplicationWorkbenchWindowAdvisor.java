a025fcba8e293015945fce4289dd7952666d3568
Torkild U. Resheim <torkildr@gmail.com>
2009-03-17 20:12:17 +0000

    Layout improvements.

--- source/no.resheim.aggregator.rcp/src/no/resheim/aggregator/rcp/ApplicationWorkbenchWindowAdvisor.java
+++ source/no.resheim.aggregator.rcp/src/no/resheim/aggregator/rcp/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,8 @@
 package no.resheim.aggregator.rcp;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -18,10 +20,22 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		return new ApplicationActionBarAdvisor(configurer);
 	}
 
+	@Override
+	public void postWindowCreate() {
+		super.postWindowCreate();
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		Shell sh = configurer.getWindow().getShell();
+		Rectangle r = sh.getDisplay().getBounds();
+		sh.setLocation(r.width - 400, r.height - 600);
+	}
+
 	public void preWindowOpen() {
 		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-		configurer.setInitialSize(new Point(600, 400));
+		configurer.setInitialSize(new Point(400, 600));
 		configurer.setShowCoolBar(false);
-		configurer.setShowStatusLine(false);
+		configurer.setShowStatusLine(true);
+		configurer.setShowFastViewBars(true);
+		configurer.setShowPerspectiveBar(false);
+		configurer.setShowProgressIndicator(true);
 	}
 }
--- source/no.resheim.aggregator.rcp/src/no/resheim/aggregator/rcp/Perspective.java
+++ source/no.resheim.aggregator.rcp/src/no/resheim/aggregator/rcp/Perspective.java
@@ -4,16 +4,15 @@ import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
+	public static final String BROWSER_ID = "org.eclipse.ui.browser.view";
 
 	private static final String MAIN_VIEW_ID = "no.resheim.aggregator.core.ui.RSSView"; //$NON-NLS-1$
 
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
-		layout.setFixed(true);
-
+		layout.setFixed(false);
 		layout.addStandaloneView(MAIN_VIEW_ID, false, IPageLayout.LEFT, 1.0f,
 				editorArea);
 	}
-
 }
