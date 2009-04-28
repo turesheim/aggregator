badda18a37a8abd472f6f186c613ab0484d2bdf6
Torkild U. Resheim <torkildr@gmail.com>
2009-04-28 21:04:34 +0000

    Formatting.

--- source/no.resheim.aggregator.google.reader/src/no/resheim/aggregator/google/reader/ui/PreferenceConstants.java
+++ source/no.resheim.aggregator.google.reader/src/no/resheim/aggregator/google/reader/ui/PreferenceConstants.java
@@ -1,6 +1,7 @@
 package no.resheim.aggregator.google.reader.ui;
 
 public class PreferenceConstants {
+	/** Preference setting key for the amount of items to download initially */
 	public static final String P_AMOUNT = "amount";
 
 }
