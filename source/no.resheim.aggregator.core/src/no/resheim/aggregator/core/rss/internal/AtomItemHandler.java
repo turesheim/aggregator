267e98f436cdf6a22211a09743c977da4956363f
Torkild U. Resheim <torkildr@gmail.com>
2009-05-07 21:53:17 +0000

    Fixed broken link handling.

--- source/no.resheim.aggregator.core/src/no/resheim/aggregator/core/rss/internal/AtomItemHandler.java
+++ source/no.resheim.aggregator.core/src/no/resheim/aggregator/core/rss/internal/AtomItemHandler.java
@@ -30,51 +30,7 @@ import org.xml.sax.SAXException;
  */
 public class AtomItemHandler extends AbstractItemHandler {
 	private static final String UPDATED = "updated"; //$NON-NLS-1$
-
-	/*
-	 * The "atom:entry" element represents an individual entry, acting as a
-	 * container for metadata and data associated with the entry. This element
-	 * can appear as a child of the atom:feed element, or it can appear as the
-	 * document (i.e., top-level) element of a stand-alone Atom Entry Document.
-	 * 
-	 * atomEntry = element atom:entry { atomCommonAttributes, (atomAuthor* &
-	 * atomCategory* & atomContent? & atomContributor* & atomId & atomLink* &
-	 * atomPublished? & atomRights? & atomSource? & atomSummary? & atomTitle &
-	 * atomUpdated & extensionElement*) }
-	 * 
-	 * This specification assigns no significance to the order of appearance of
-	 * the child elements of atom:entry.
-	 * 
-	 * The following child elements are defined by this specification (note that
-	 * it requires the presence of some of these elements):
-	 * 
-	 * o atom:entry elements MUST contain one or more atom:author elements,
-	 * unless the atom:entry contains an atom:source element that contains an
-	 * atom:author element or, in an Atom Feed Document, the atom:feed element
-	 * contains an atom:author element itself. o atom:entry elements MAY contain
-	 * any number of atom:category elements. o atom:entry elements MUST NOT
-	 * contain more than one atom:content element. o atom:entry elements MAY
-	 * contain any number of atom:contributor elements. o atom:entry elements
-	 * MUST contain exactly one atom:id element. o atom:entry elements that
-	 * contain no child atom:content element MUST contain at least one atom:link
-	 * element with a rel attribute value of "alternate". o atom:entry elements
-	 * MUST NOT contain more than one atom:link element with a rel attribute
-	 * value of "alternate" that has the same combination of type and hreflang
-	 * attribute values. o atom:entry elements MAY contain additional atom:link
-	 * elements beyond those described above. o atom:entry elements MUST NOT
-	 * contain more than one atom:published element. o atom:entry elements MUST
-	 * NOT contain more than one atom:rights element. o atom:entry elements MUST
-	 * NOT contain more than one atom:source element. o atom:entry elements MUST
-	 * contain an atom:summary element in either of the following cases: the
-	 * atom:entry contains an atom:content that has a "src" attribute (and is
-	 * thus empty). the atom:entry contains content that is encoded in Base64;
-	 * i.e., the "type" attribute of atom:content is a MIME media type
-	 * [MIMEREG], but is not an XML media type [RFC3023], does not begin with
-	 * "text/", and does not end with "/xml" or "+xml". o atom:entry elements
-	 * MUST NOT contain more than one atom:summary element. o atom:entry
-	 * elements MUST contain exactly one atom:title element. o atom:entry
-	 * elements MUST contain exactly one atom:updated element.
-	 */
+	// TODO: Properly handle links within content
 	private static final String ENTRY = "entry"; //$NON-NLS-1$
 
 	private static final String ISSUED = "issued"; //$NON-NLS-1$
@@ -101,28 +57,19 @@ public class AtomItemHandler extends AbstractItemHandler {
 		// Mandatory
 		if (qName.equals(TITLE)) {
 			item.setTitle(getBuffer().toString());
-			setCapture(false);
-		}
-		if (qName.equals(LINK)) {
-			item.setLink(getBuffer().toString());
-			setCapture(false);
 		}
 		if (qName.equals(SUMMARY)) {
 			item.internalSetText(getBuffer().toString());
-			setCapture(false);
 		}
 		if (qName.equals(CONTENT)) {
 			item.internalSetText(getBuffer().toString());
-			setCapture(false);
 		}
 		if (qName.equals(ID)) {
 			item.setGuid(getBuffer().toString());
-			setCapture(false);
 		}
 		// RFC822 date specification
 		if (qName.equals(ISSUED)) {
 			item.setPublicationDate(parse(getBuffer().toString()).getTime());
-			setCapture(false);
 		}
 		if (qName.equals(ENTRY)) {
 			if (!collection.hasArticle(item.getGuid())) {
@@ -138,6 +85,8 @@ public class AtomItemHandler extends AbstractItemHandler {
 				|| qName.equals(ISSUED) || qName.equals(ID)
 				|| qName.equals(CONTENT) || qName.equals(UPDATED)) {
 			setCapture(true);
+		} else {
+			setCapture(false);
 		}
 		if (qName.equals("source")) {
 			return new AtomItemSourceHandler(collection, feed);
